059f416ecefa85a5da70e745dde67ed575eee876
Student ApnaCollege <suhaniv593@gmail.com>
2026-05-20 19:54:03 +0530

    Added new feature

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-print("===== DSA Progress Tracker=====")
-while True:
-    name =  input("Enter Solved question name (or type exist): ")
-    if name.lower() == "exit":
-        print("Progress saved. Existinng...")
-        break  
-    print("Question Saved:", name)
+question = input("Enter solved question name:")
+difficulty = input("Enter difficulty(Easy/Medium/Hard):")
+topic = input("Enter topic:")
+file= open("progress.txt", "a")
+file.write(question + " - " + difficulty + " - " + topic + "\n")
+file.close()
+print("Question Saved Successfully!")
+print("\nYour Saved Progress:\n")
+file = open("progress.txt", "r")
+data = file.read()
+print(data)
+file.close()
+search = input("Enter question to search:")
+file = open("progress.txt", "r")
+data = file.read()
+if search in  data:
+    print("Question Found!")
+else:
+    print("Question Not Found!")
+file.close()
+
+
